97ff39fd4c36266354455cd912eae13de030b812
Marwa <marwaqaisiya@gmail.com>
2025-03-19 10:37:24 +0300

    Modal to show user photo is added

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -20,7 +20,11 @@
         <tbody>
             @foreach($users as $user)
                 <tr>
-                    <td><img src="{{ $user->photo }}" alt="User Photo" width="50" height="50"></td>
+                    <td>
+                        <img src="{{ $user->photo }}" alt="User Photo" width="50" height="50" 
+                            class="img-thumbnail user-photo" data-bs-toggle="modal" 
+                            data-bs-target="#photoModal" data-photo="{{ $user->photo }}">
+                    </td>
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->gender }}</td>
@@ -29,12 +33,11 @@
                     <td>
                         <a href="{{ route('users.edit', $user->id) }}" class="btn btn-sm btn-warning">Edit</a>
                         <form action="{{ route('users.destroy', $user->id) }}" method="POST" style="display:inline;" 
-                        onsubmit="return confirm('Are you sure you want to delete this user?');">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+                            onsubmit="return confirm('Are you sure you want to delete this user?');">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
                         </form>
-
                     </td>
                 </tr>
             @endforeach
@@ -45,4 +48,34 @@
     <div class="d-flex justify-content-center">
         {{ $users->links() }}
     </div>
+
+    <!-- Modal for displaying user photo -->
+    <div class="modal fade" id="photoModal" tabindex="-1" aria-labelledby="photoModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="photoModalLabel">User Photo</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-center">
+                    <img id="modalPhoto" src="" class="img-fluid rounded" alt="User Photo">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- JavaScript to handle the image modal -->
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const photoModal = document.getElementById("photoModal");
+            const modalPhoto = document.getElementById("modalPhoto");
+
+            document.querySelectorAll(".user-photo").forEach(photo => {
+                photo.addEventListener("click", function () {
+                    const photoSrc = this.getAttribute("data-photo");
+                    modalPhoto.src = photoSrc;
+                });
+            });
+        });
+    </script>
 @endsection
